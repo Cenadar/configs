aef25833e26e01c96e6fe33e1f564df90649d71b
Maksym Bevza <cenadar16@gmail.com>
2014-03-28 12:18:12 +0200

    Added checking for dayly/weekly/monthly

--- sh/arc.sh
+++ sh/arc.sh
@@ -1,13 +1,61 @@
 #!/bin/bash
 
+function get_date {
+  if [ -z $1 ]
+  then
+    opt=0
+  else
+    opt=$1
+  fi
+  date --utc +%Y-%m-%d --date="-$opt day"
+}
+
 backup_dir=$(readlink ~/backups)
-date_stamp=$(date --utc +%Y-%m-%d)
+date_stamp=$(get_date)
 time_stamp=$(date --utc +%H-%M-%S)
 
 dir="$backup_dir/$date_stamp"
 file="$dir/$time_stamp-home.tgz"
 logfile="$dir/$time_stamp.log"
-backup_list="$backup_dir/1.list.sh"
+
+dayly_found=false
+weekly_found=false
+monthly_found=false
+
+for i in {0..30}
+do
+  if [ -d "$backup_dir/$(get_date $i)" ]; then
+    if [ $i -lt 1 ]; then
+      dayly_found=true
+    fi
+    if [ $i -lt 7 ]; then
+      weekly_found=true
+    fi
+    if [ $i -lt 30 ]; then
+      monthly_found=true
+    fi
+  fi
+done
+
+if [ "$monthly_found" = false ]; then
+list="monthly-list"
+else
+  if [ "$weekly_found" = false ]; then
+    list="weekly-list"
+  else
+    if [ "$dayly_found" = false ]; then
+      list="dayly-list"
+    else
+      echo "$dayly_found"
+      exit
+    fi
+  fi
+fi
+
+backup_list="$backup_dir/$list.sh"
+
+echo "$backup_list"
+exit
 
 mkdir -p $dir
 rc=$?
@@ -23,11 +71,9 @@ then
 fi
 
 t1=$(date +"%s")
-opt=""
-#opt="--use-compress-program=pigz"
 ~/code/delo.sh
 ~/prog/delo.sh
-$backup_list | xargs tar cvfz $opt $file | tee $logfile
+$backup_list | xargs tar cvfz $file | tee $logfile
 t2=$(date +"%s")
 
 t_diff=$(($t2-$t1))
